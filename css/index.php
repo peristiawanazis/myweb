b48e9464fef0703c0aadb61d4755c5c829be2dbd
peristiawanazis <peristiawanazis@gmail.com>
2014-08-05 22:19:13 +0700

    first

---
+++ css/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+| -------------------------------------------------------------------
+| CSS Redirect
+| -------------------------------------------------------------------
+| File ini berisi fungsi untuk me-redirect apabila ada yang mencoba
+| mengakses langsung folder css ini.
+|
+*/
+	header ('Location: http://localhost:8080/ibnu/');
+
+/* End of file index.php */
+/* Location: ./css/index.php */
